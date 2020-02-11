8cb23f22b2c10fab42a9866fbb4e84c8d17e9593
lxj <877061723@qq.com>
2020-02-11 13:33:16 +0800

    flyway

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,9 @@
+create table if not exists user
+(
+	id int auto_increment primary key,
+	account_id varchar(100),
+	name varchar(50),
+	token char(36),
+	gmt_create bigint,
+	gmt_modified bigint
+);
